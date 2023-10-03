f514c77cb0e25d54889e68d5a5abd5780e5122c1
Darren Draper <darren.j.draper@gmail.com>
2023-10-03 22:12:30 +1000

    Implemented routing functionality for Activity 3

--- Module2/Activity3/app.js
+++ Module2/Activity3/app.js
@@ -1,12 +1,22 @@
 // Import the 'http' module
 const http = require("http");
 
+function routing(req, res) {
+    const url = req.url;
+
+    if (url.startsWith("/form")) { // The form page
+        res.write("Form");
+        res.end();
+    } else if (url.startsWith("/add")) { // The add page
+        res.write("Add");
+        res.end();
+    } else { // No page matched the url
+        res.write("No matching page");
+        res.end();
+    }
+}
+
 // create a server object
-http
-    .createServer(function (req, res) {
-        res.write("Hello World!"); // write a response
-        res.end(); // end the response
-    })
-    .listen(3000, function () {
-        console.log("server start at port 3000"); // the server object listens on port 3000
-    });
+http.createServer(routing).listen(3000, function () {
+    console.log("server start at port 3000"); // the server object listens on port 3000
+});
