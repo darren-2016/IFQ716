f7cbaaca13f066326bbc5e3acc6196aa7a96af36
Darren Draper <darren.j.draper@gmail.com>
2023-10-08 21:34:03 +1000

    Added read functionality

--- Module2/Activity6/app.js
+++ Module2/Activity6/app.js
@@ -2,13 +2,60 @@
 // Add some more fields to our guestbook and add a guestbook route
 
 // Import the 'http' and 'fs' modules
-const http = require("http");
-const fs = require("fs");
-
-const path = "guestBook.json";
+import { writeFile, readFile } from 'fs';
+import * as http from 'http';
 
 
-function routing(req, res) {
+const path = "./guestBook.json";
+
+/**
+ * Helper function for reading the JSON file
+ * @param {string} path File path to JSON file
+ * @return {}  
+ */
+async function readFileAsync(path) {
+    return new Promise((resolve, reject) => {
+        readFile(path, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(data);
+        });
+    });
+}
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @returns None
+ */
+async function readGuestbook(req, res) {
+
+    try {
+        const response = await readFileAsync(path);
+        console.table(JSON.parse(response));
+
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(JSON.parse(response)));
+        res.statusCode = 200;
+        return res.end();
+    } catch (error) {
+        console.log(error);
+        res.statusCode = 500;
+        res.write('Failed to load JSON file');
+        return res.end();
+    }
+}
+
+
+/**
+ * Routing function
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+async function routing(req, res) {
     const url = req.url;
 
     if (url.startsWith("/form")) { // The form page
@@ -27,8 +74,9 @@ function routing(req, res) {
             </form>
         `);
         res.end();
-    } else if (url.startsWith("/add")) { // The add page
-        fs.readFile(path, function (err, data) {
+    } 
+    else if (url.startsWith("/add")) { // The add page
+        readFile(path, function (err, data) {
             if (err) {
                 res.write("You should do some real error handling here");
                 res.end();
@@ -70,7 +118,7 @@ function routing(req, res) {
                 
                 // Write the updated guestbook to the filesystem
                 console.log('Write to guestbook');
-                fs.writeFile(path, JSON.stringify(guestBook), (err) => {
+                writeFile(path, JSON.stringify(guestBook), (err) => {
                     if (err) {
                         console.log('Error writing to file');
                         res.write("You should do some real error handling here");
@@ -83,18 +131,23 @@ function routing(req, res) {
                 });
             });
         });
-    } else if (url.startsWith("/read")) {
-        res.write("READ");
-        // res.writeHead();
-        res.end();
+    } else if (url.startsWith("/read")) { // read from guestbook JSON data file and display contents
+        let guestBook = await readGuestbook(req, res);
 
+        // console.log(guestBook);
+        // return;            
+        
+        
+        res.end();
     } else { // No page matched the url
         res.write("No matching page");
         res.end();
     }
 }
 
-// create a server object
+/**
+ * Create a server object
+ */
 http.createServer(routing).listen(3000, function () {
     console.log("server start at port 3000"); // the server object listens on port 3000
 });
