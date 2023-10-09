204e2f673ecfaf8c5365d366499ead419367b73b
Darren Draper <darren.j.draper@gmail.com>
2023-10-09 22:19:43 +1000

    Implemented client JavaScript - the hacky way!

--- Module3/Activity1/app.js
+++ Module3/Activity1/app.js
@@ -1,4 +1,30 @@
-let http = require("http");
+const fs = require("fs");
+const http = require("http");
+
+const data = [
+    "Siamese",
+    "Persian",
+    "Maine Coon",
+    "Bengal",
+    "Scottish Fold",
+    "British Shorthair",
+    "Sphynx",
+    "Abyssinian",
+    "American Shorthair",
+    "Russian Blue",
+    "Ragdoll",
+    "Devon Rex",
+    "Birman",
+    "Siberian",
+    "Manx",
+    "Exotic Shorthair",
+    "Burmese",
+    "Tonkinese",
+    "Savannah",
+    "Himalayan"    
+];
+
+
 function routing(req, res) {
     const url = req.url;
     const method = req.method;
@@ -6,28 +32,45 @@ function routing(req, res) {
     if (url.startsWith("/data")) {
         if (method == "GET") {
             // The form page
-            res.write("Data");
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.write(JSON.stringify(data));
             res.end();
-            return;
+            // return;
         }
     } else if (url.startsWith("/login")) {
         if (method == "POST") {
             // The add page
             res.write("Login");
             res.end();
-            return;
+            // return;
         }
     } else if (url.startsWith("/client")) {
         if (method == "GET") {
             // The add page
-            res.write("Client");
-            res.end();
-            return;
+            const filename = "client.html"; // The filename to read from
+
+            // Try to read the file
+            fs.readFile(filename, "binary", function (err, file) {
+                // If there is an error, output the message as JSON and return
+                if (err) {
+                    res.writeHead(500, { "Content-Type": "application/json" });
+                    res.write(JSON.stringify( { error: err }));
+                    res.end();
+                    return;
+                }
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.write(file, "binary");
+                res.end();
+            });
+            // res.write("Client");
+            // res.end();
+            // return;
         }
+    } else {
+        // No page matched the url
+        res.write("No matching page");
+        res.end();
     }
-    // No page matched the url
-    res.write("No matching page");
-    res.end();
 }
 
 http.createServer(routing).listen(3000, function () {
