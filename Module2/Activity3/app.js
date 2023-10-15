228b502d36153ff51d217c16707d44f7793c76d2
Darren Draper <darren.j.draper@gmail.com>
2023-10-15 18:47:19 +1000

    Added comments

--- Module2/Activity3/app.js
+++ Module2/Activity3/app.js
@@ -1,6 +1,17 @@
+/**
+ * IFQ716 - Module 2
+ * Activity 3: Basic routing
+ * 
+ */
+
 // Import the 'http' module
 const http = require("http");
 
+/**
+ * Routing function
+ * @param {Object} req Request
+ * @param {Object} res Response
+ */
 function routing(req, res) {
     const url = req.url;
 
@@ -16,7 +27,9 @@ function routing(req, res) {
     }
 }
 
-// create a server object
+/**
+ * Create a server object, with the routing function as its parameter
+ */
 http.createServer(routing).listen(3000, function () {
     console.log("server start at port 3000"); // the server object listens on port 3000
 });
