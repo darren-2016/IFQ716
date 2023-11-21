1f2a7e88a123f3c74d39a0b9a2a312a64d98fc98
Darren Draper <darren.j.draper@gmail.com>
2023-11-21 12:14:38 +1000

    Implemented results limit and sorting for both routes

--- Module5/Activity2/expworld/routes/index.js
+++ Module5/Activity2/expworld/routes/index.js
@@ -7,9 +7,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/api/city", function (req, res, next) {
+  const limit = parseInt(req.query.limit) || 10; // optional limit parameter, default to 10 if not provided
+  const sortBy = req.query.sortBy || "name"; // set a default sort by name if not provided
+  const sortOrder = req.query.sortOrder === "desc" ? "desc" : "asc"; // set the sort order
+
   req.db
     .from("city")
     .select("name", "district")
+    .limit(limit) // apply a limit
+    .orderBy(sortBy, sortOrder) // apply the sorting
     .then((rows) => {
       res.json({ Error: false, Message: "Success", City: rows });
     })
@@ -20,10 +26,16 @@ router.get("/api/city", function (req, res, next) {
 });
 
 router.get("/api/city/:CountryCode", function (req, res, next) {
+  const limit = parseInt(req.query.limit) || 10; // optional limit parameter, default to 10 if not provided
+  const sortBy = req.query.sortBy || "name"; // set a default sort by name if not provided
+  const sortOrder = req.query.sortOrder === "desc" ? "desc" : "asc"; // set the sort order
+
   req.db
     .from("city")
     .select("*")
+    .limit(limit) // apply a limit
     .where("CountryCode", "=", req.params.CountryCode)
+    .orderBy(sortBy, sortOrder) // apply the sorting
     .then((rows) => {
       res.json({ Error: false, Message: "Success", City: rows });
     })
